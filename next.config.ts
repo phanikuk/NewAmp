45c9da57952b1444c30744ad798701bbea41b995
phanikuk <phanikuk@amazon.com>
2026-03-25 16:02:28 +0000

    Adding config values in next config ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   env: {
+    CW_ACCESS_KEY_ID: process.env.CW_ACCESS_KEY_ID,
+    CW_SECRET_ACCESS_KEY: process.env.CW_SECRET_ACCESS_KEY,
+    CW_REGION: process.env.CW_REGION,
+    CLOUDWATCH_LOG_GROUP: process.env.CLOUDWATCH_LOG_GROUP,
+  },
 };
 
 export default nextConfig;
